47ffd0f78cc05d698e1991c2981f783387e0cfcb
Dominic Weibel <doweibel@student.ethz.ch>
2019-05-07 17:01:31 +0200

    first commit

---
+++ src/GamePanel.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    private static final long serialVersionUID = 1L;
+
+    JFrame frame;
+
+    long delta = 0;     //Zeit des letzten Durchlauufs der GameLoop
+    long last = 0;      //Speicherung der letzten Systemzeit
+    long fps = 0;       //Berechnung FPS
+
+
+    public  GamePanel(int w, int h){
+        this.setPreferredSize(new Dimension(w,h));
+        frame = new JFrame("GameFrame");
+        frame.setLocation(100,100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(this);
+        frame.pack();
+        frame.setVisible(true);
+
+        doInitializations();
+
+        //Thread stuff
+        Thread th = new Thread(this);
+        th.start();
+    }
+
+    //do one time initializations (load images, ...)
+    private void doInitializations() {
+        last = System.nanoTime();
+
+    }
+
+
+    @Override
+    public void run() {
+        while(frame.isVisible()){
+            computeDelta();
+
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    private void computeDelta() {
+        delta = System.nanoTime() - last;
+        last = System.nanoTime();
+        fps = ((long) 1e9) / delta;
+    }
+}
